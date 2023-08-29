de229af416ddbe3e100cf3f912020ecff45b0f55
George Ion <89georgeion@gmail.com>
2023-08-29 10:29:42 +0300

    setup files and stuff

---
+++ src/Components/Common/Footer.jsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <>
+      <div className="footer-primary ">
+        <div className="footer-primary-inner container">
+          <nav className="footer-nav">hello </nav>
+
+          <section className="footer-contact">hello </section>
+
+          <section className="footer-connect">hello </section>
+        </div>
+      </div>
+
+      <div className="footer-secondary">hell</div>
+    </>
+  );
+}
---
+++ src/Components/Common/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <>
+      <section className="header-secondary">test</section>
+      <section className="header-primary d-none d-lg-block">
+        <nav className="nav-primary"></nav>
+
+        <nav className="nav-secondary"></nav>
+      </section>
+    </>
+  );
+}
